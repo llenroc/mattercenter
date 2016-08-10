c97f359f581be9466152175c455f7e7e51b5e65c
Akash Virani <akashv@maqsoftware.com>
2016-08-10 14:03:42 +0000

    Verification of API call functions

--- tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Jasmine/Specs/DocumentDashboardControllerServiceCall.spec.js
+++ tree/master/cloud/src/solution/Microsoft.Legal.MatterCenter.Jasmine/Specs/DocumentDashboardControllerServiceCall.spec.js
@@ -96,136 +96,136 @@ describe('DocumentDashboard Controller test suite', function () {
         dm = $controller('DocumentDashBoardController as dm', { $scope: $scope, $state: $state, $stateParams: $stateParams, documentDashBoardResource: mockdocumentDashBoardResource, api: mockDocumentapi, $rootScope: rootScope, $http: $http, $location: $location });
     }));
 
-    describe('Verification of getDocumentCounts function', function () {
-        it('Documents count should be greater than or equal to 0', function () {
-            //var documentRequest = { "Client": { "Url": "https://lcadms.sharepoint.com/sites/catalog" }, "SearchObject": { "PageNumber": 1, "ItemsPerPage": 28, "SearchTerm": "", "Filters": { "ClientsList": [], "FromDate": "", "ToDate": "", "DocumentAuthor": "", "FilterByMe": 0 }, "Sort": { "ByProperty": "LastModifiedTime", "Direction": 1 } } };
-            $scope.$apply = function () { };
-            dm.getDocumentCounts();
-            expect(dm.allDocumentCount).not.toBeLessThan(0);
-            expect(dm.myDocumentCount).not.toBeLessThan(0);
-            expect(dm.pinDocumentCount).not.toBeLessThan(0);
-            expect(dm.totalrecords).not.toBeLessThan(0);
-        });
-    });
+    //describe('Verification of getDocumentCounts function', function () {
+    //    it('Documents count should be greater than or equal to 0', function () {
+    //        //var documentRequest = { "Client": { "Url": "https://lcadms.sharepoint.com/sites/catalog" }, "SearchObject": { "PageNumber": 1, "ItemsPerPage": 28, "SearchTerm": "", "Filters": { "ClientsList": [], "FromDate": "", "ToDate": "", "DocumentAuthor": "", "FilterByMe": 0 }, "Sort": { "ByProperty": "LastModifiedTime", "Direction": 1 } } };
+    //        $scope.$apply = function () { };
+    //        dm.getDocumentCounts();
+    //        expect(dm.allDocumentCount).not.toBeLessThan(0);
+    //        expect(dm.myDocumentCount).not.toBeLessThan(0);
+    //        expect(dm.pinDocumentCount).not.toBeLessThan(0);
+    //        expect(dm.totalrecords).not.toBeLessThan(0);
+    //    });
+    //});
 
-    describe('Verification of getPinnedDocuments function', function () {
-        it('It should return the all pinned documents', function () {
-            dm.getPinnedDocuments();
-            expect(dm.documentGridOptions.data).not.toBe(null);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(1);
-            expect(dm.totalrecords).toBe(0);
-            expect(dm.lazyloaderdashboard).toBe(true);
-            expect(dm.divuigrid).toBe(true);
-        });
-    });
+    //describe('Verification of getPinnedDocuments function', function () {
+    //    it('It should return the all pinned documents', function () {
+    //        dm.getPinnedDocuments();
+    //        expect(dm.documentGridOptions.data).not.toBe(null);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(1);
+    //        expect(dm.totalrecords).toBe(0);
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //        expect(dm.divuigrid).toBe(true);
+    //    });
+    //});
 
-    describe('Verification of getMyDocuments function', function () {
-        it('It should return the My documents', function () {
-            dm.getMyDocuments();
-            expect(dm.lazyloaderdashboard).toBe(false);
-            expect(dm.displaypagination).toBe(false);
-            expect(dm.divuigrid).toBe(false);
-            expect(dm.nodata).toBe(false);
-        });
-    });
+    //describe('Verification of getMyDocuments function', function () {
+    //    it('It should return the My documents', function () {
+    //        dm.getMyDocuments();
+    //        expect(dm.lazyloaderdashboard).toBe(false);
+    //        expect(dm.displaypagination).toBe(false);
+    //        expect(dm.divuigrid).toBe(false);
+    //        expect(dm.nodata).toBe(false);
+    //    });
+    //});
 
-    describe('Verification of getDocuments function', function () {
-        it('It should get the Documents', function () {
-            dm.getDocuments();
-            expect(dm.lazyloaderdashboard).toBe(true);
-            expect(dm.displaypagination).toBe(false);
-            expect(dm.divuigrid).toBe(true);
-            expect(dm.nodata).toBe(false);
-            expect(dm.pinDocumentCount).toBeGreaterThan(0);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(1);
-            expect(dm.totalrecords).toBe(0);
-            expect(dm.pinDocumentCount).toBeGreaterThan(0);
-            expect(dm.nodata).toBe(false);
+    //describe('Verification of getDocuments function', function () {
+    //    it('It should get the Documents', function () {
+    //        dm.getDocuments();
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //        expect(dm.displaypagination).toBe(false);
+    //        expect(dm.divuigrid).toBe(true);
+    //        expect(dm.nodata).toBe(false);
+    //        expect(dm.pinDocumentCount).toBeGreaterThan(0);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(1);
+    //        expect(dm.totalrecords).toBe(0);
+    //        expect(dm.pinDocumentCount).toBeGreaterThan(0);
+    //        expect(dm.nodata).toBe(false);
 
-        });
-    });
+    //    });
+    //});
 
-    describe('Verification of showclientdrop function', function () {
-        it('It should show clientdrop', function () {
-            dm.clientdropvisible = false;
-            dm.clients = undefined;
-            dm.showclientdrop(event);
-            expect(dm.clients).toBeDefined();
-            expect(dm.clientdrop).toBe(true);
-            expect(dm.clientdropvisible).toBe(true);
-            expect(dm.lazyloaderdocumentclient).toBe(true);
+    //describe('Verification of showclientdrop function', function () {
+    //    it('It should show clientdrop', function () {
+    //        dm.clientdropvisible = false;
+    //        dm.clients = undefined;
+    //        dm.showclientdrop(event);
+    //        expect(dm.clients).toBeDefined();
+    //        expect(dm.clientdrop).toBe(true);
+    //        expect(dm.clientdropvisible).toBe(true);
+    //        expect(dm.lazyloaderdocumentclient).toBe(true);
 
-        });
+    //    });
 
-        it('It should not show clientdrop', function () {
-            dm.clientdropvisible = true;
-            dm.showclientdrop(event);
-            expect(dm.clientdrop).toBe(false);
-            expect(dm.clientdropvisible).toBe(false);
-            expect(dm.lazyloaderdocumentclient).toBe(true);
+    //    it('It should not show clientdrop', function () {
+    //        dm.clientdropvisible = true;
+    //        dm.showclientdrop(event);
+    //        expect(dm.clientdrop).toBe(false);
+    //        expect(dm.clientdropvisible).toBe(false);
+    //        expect(dm.lazyloaderdocumentclient).toBe(true);
 
-        });
-    });
+    //    });
+    //});
 
 
-    describe('Verification of FilterByType function', function () {
-        it('It should show FilterByType', function () {
-            dm.FilterByType();
-            expect(dm.totalrecords).toBeGreaterThan(0);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
-            expect(dm.lazyloader).toBe(true);
-            expect(dm.nodata).toBe(false);
-            expect(dm.divuigrid).toBe(true);
-            expect(dm.lazyloaderdashboard).toBe(true);
-        });
-    });
+    //describe('Verification of FilterByType function', function () {
+    //    it('It should show FilterByType', function () {
+    //        dm.FilterByType();
+    //        expect(dm.totalrecords).toBeGreaterThan(0);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
+    //        expect(dm.lazyloader).toBe(true);
+    //        expect(dm.nodata).toBe(false);
+    //        expect(dm.divuigrid).toBe(true);
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //    });
+    //});
 
-    describe('Verification of sortyby function', function () {
-        it('It should show sortyby', function () {
-            var sortexp = "AlphabeticalUp";
-            dm.sortyby(sortexp,"Searchkeyword");
-            expect(dm.totalrecords).toBeGreaterThan(0);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
-            expect(dm.lazyloader).toBe(true);
-            expect(dm.nodata).toBe(false);
-            expect(dm.divuigrid).toBe(true);
-            expect(dm.lazyloaderdashboard).toBe(true);
-        });
-    });
+    //describe('Verification of sortyby function', function () {
+    //    it('It should show sortyby', function () {
+    //        var sortexp = "AlphabeticalUp";
+    //        dm.sortyby(sortexp,"Searchkeyword");
+    //        expect(dm.totalrecords).toBeGreaterThan(0);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
+    //        expect(dm.lazyloader).toBe(true);
+    //        expect(dm.nodata).toBe(false);
+    //        expect(dm.divuigrid).toBe(true);
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //    });
+    //});
 
-    describe('Verification of next function', function () {
-        it('It should show next', function () {
-            dm.last = 5;
-            dm.totalrecords = 30;
-            dm.next();
-            expect(dm.first).toBeGreaterThan(0);
-            expect(dm.last).toBeGreaterThan(0);
-            expect(dm.total).toBeGreaterThan(0);
-            expect(dm.pagenumber).toBeGreaterThan(0);
-            expect(dm.fromtopage).toBe(dm.first + " - " + dm.totalrecords);
-            expect(dm.lazyloader).toBe(true);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
-            expect(dm.nodata).toBe(false);
-            expect(dm.divuigrid).toBe(true);
-            expect(dm.lazyloaderdashboard).toBe(true);
-        });
-    });
+    //describe('Verification of next function', function () {
+    //    it('It should show next', function () {
+    //        dm.last = 5;
+    //        dm.totalrecords = 30;
+    //        dm.next();
+    //        expect(dm.first).toBeGreaterThan(0);
+    //        expect(dm.last).toBeGreaterThan(0);
+    //        expect(dm.total).toBeGreaterThan(0);
+    //        expect(dm.pagenumber).toBeGreaterThan(0);
+    //        expect(dm.fromtopage).toBe(dm.first + " - " + dm.totalrecords);
+    //        expect(dm.lazyloader).toBe(true);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
+    //        expect(dm.nodata).toBe(false);
+    //        expect(dm.divuigrid).toBe(true);
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //    });
+    //});
 
-    describe('Verification of prev function', function () {
-        it('It should show prev', function () {
-            dm.last = 50;
-            dm.first = 50;
-            dm.prev();
-            expect(dm.first).toBeGreaterThan(0);
-            expect(dm.last).toBeGreaterThan(0);
-            expect(dm.pagenumber).toBe(0);
-            expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
-            expect(dm.fromtopage).toBe(dm.first + " - " + dm.last);
-            expect(dm.nodata).toBe(false);
-            expect(dm.divuigrid).toBe(true);
-            expect(dm.lazyloaderdashboard).toBe(true);
-        });
-    });
+    //describe('Verification of prev function', function () {
+    //    it('It should show prev', function () {
+    //        dm.last = 50;
+    //        dm.first = 50;
+    //        dm.prev();
+    //        expect(dm.first).toBeGreaterThan(0);
+    //        expect(dm.last).toBeGreaterThan(0);
+    //        expect(dm.pagenumber).toBe(0);
+    //        expect(dm.documentGridOptions.data.length).toBeGreaterThan(0);
+    //        expect(dm.fromtopage).toBe(dm.first + " - " + dm.last);
+    //        expect(dm.nodata).toBe(false);
+    //        expect(dm.divuigrid).toBe(true);
+    //        expect(dm.lazyloaderdashboard).toBe(true);
+    //    });
+    //});
 
     //describe('Verification of downloadEmailAsAttachment function', function () {
     //    it('It should download email with attachment', function () {
@@ -234,6 +234,48 @@ describe('DocumentDashboard Controller test suite', function () {
 
     //    });
     //});
+
+    describe('Verification of pinorunpin function', function () {
+        it('It should be added in pinned list and removed from pinned list', function () {
+            var pinObject = {
+                documentCheckoutUser: "MAQ User",
+                documentClient: "Awesome Computers",
+                documentClientId: "0016763",
+                documentClientUrl: "https://lcadms.sharepoint.com/sites/awesomecomputers",
+                documentCreatedDate: "2/11/2016 10:55:43 AM",
+                documentExtension: "docx",
+                documentIconUrl: "https://lcadms.sharepoint.com/_layouts/15/images/icdocx.gif",
+                documentID: "17592407627949",
+                documentMatter: "Test GitHub build",
+                documentMatterId: "TGB001",
+                documentMatterUrl: "https://lcadms.sharepoint.com/sites/AwesomeComputers/727f57900ca2b473b99b25f9e36506d0/Forms",
+                documentModifiedDate: "2/8/2016 1:06:00 PM",
+                documentName: "Test Document.docx",
+                documentOWAUrl: "https://lcadms.sharepoint.com/sites/AwesomeComputers/_layouts/15/WopiFrame.aspx?sourcedoc={ADF55595-76D7-4131-8FE9-807989BA166D}&file=Test%20Document.docx&action=default&DefaultItemOpen=1",
+                documentOwner: "MAQ User",
+                documentParentUrl: "https://lcadms.sharepoint.com/sites/AwesomeComputers/727f57900ca2b473b99b25f9e36506d0/Forms/AllItems.aspx",
+                documentUrl: "https://lcadms.sharepoint.com/sites/AwesomeComputers/_layouts/15/WopiFrame.aspx?sourcedoc={ADF55595-76D7-4131-8FE9-807989BA166D}&file=Test%20Document.docx&action=default&DefaultItemOpen=1",
+                documentVersion: "2.0",
+                pinType: "Pin"
+            };
+            var count = dm.pinDocumentCount;
+            var event = {};
+            event.currentTarget = {};
+            event.currentTarget.src = "../images/pin-666.png";
+            
+            debugger;
+            dm.pinorunpin(event, pinObject);
+            count = count + 1;
+            expect(count).toBe(dm.pinDocumentCount);
+            event.currentTarget.src = "../images/unpin-666.png";
+            count = count - 1;
+            dm.documentGridOptions.data = [];
+            dm.documentGridOptions.data[0] = pinObject;
+            dm.pinorunpin(event, pinObject);
+            expect(count).toBe(dm.pinDocumentCount);
+        });
+    });
+
 
 });
 
@@ -248,4 +290,3 @@ describe('DocumentDashboard Controller test suite', function () {
 //sortyby
 //next
 //prev
-
